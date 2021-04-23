6acd3251be7230bc69febfc5b7e0c59d1dd4d9ae
baiyajin <baiyajin110@qq.com>
2021-04-23 23:03:06 +0800

    增加车机端控制台显示

--- src/main.js
+++ src/main.js
@@ -7,6 +7,10 @@ import router from './router'
 import Vant from 'vant'
 import 'vant/lib/index.css'
 import '@vant/touch-emulator'
+// 手机端调试神器
+import Vconsole from 'vconsole'
+// eslint-disable-next-line no-unused-vars
+let vConsole = new Vconsole()
 Vue.use(Vant)
 Vue.config.productionTip = false
 /* eslint-disable no-new */
