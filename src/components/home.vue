d571eab332a80048a595db0a42360c4cd2f9adee
baiyajin <baiyajin110@qq.com>
2025-09-25 23:51:46 +0800

    采用element-plus重构

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,23 +1,37 @@
 <template>
   <div>
-    <van-button @click="show = !show" plain hairline type="primary" style="border-color:#8098ff;color:#8098ff;">开始破解</van-button>
-    <van-button v-if="src" @click="checkCode" plain hairline type="primary" style="border-color:#8098ff;color:#8098ff;">查看原代码</van-button>
-    <van-dialog v-model:show="show" title="授权码" show-cancel-button @confirm="submitCode">
-      <van-form>
-        <van-field v-on:input="validator" clearable v-model="authorizationCode" input-align="center" placeholder="请输入授权码"/>
-      </van-form>
-    </van-dialog>
+    <el-button @click="show = true" type="primary" plain>开始破解</el-button>
+    <el-button v-if="src" @click="checkCode" type="primary" plain>查看原代码</el-button>
+    <el-dialog v-model="show" title="授权码" width="420">
+      <el-form @submit.prevent>
+        <el-form-item>
+          <el-input v-model="authorizationCode" @input="validator" placeholder="请输入授权码" clearable />
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button @click="show = false">取 消</el-button>
+          <el-button type="primary" @click="submitCode">确 定</el-button>
+        </span>
+      </template>
+    </el-dialog>
 
-    <van-form style="margin-top: 100px;">
-      <van-field clearable v-model="code.device_id" input-align="center" placeholder="请输入设备ID"/>
-      <van-button @click="on_button_click" plain hairline type="primary" style="border-color:#8098ff;color:#8098ff;">生成注册码</van-button>
-      <van-field clearable v-model="code.registration_code" input-align="center"/>
-    </van-form>
+    <el-form style="margin-top: 100px;" @submit.prevent>
+      <el-form-item>
+        <el-input v-model="code.device_id" placeholder="请输入设备ID" clearable />
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="on_button_click" type="primary" plain>生成注册码</el-button>
+      </el-form-item>
+      <el-form-item>
+        <el-input v-model="code.registration_code" readonly clearable />
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
-import { showToast } from 'vant'
+import { ElNotification } from 'element-plus'
 export default {
   data () {
     return {
@@ -37,7 +51,13 @@ export default {
   watch: {
     'authorizationCode': {
       handler (n, o) {
-        showToast(this.toast)
+        ElNotification({
+          title: '提示',
+          message: this.toast && this.toast.message ? this.toast.message : '输入已更新',
+          type: 'info',
+          position: 'bottom-right',
+          duration: 1500
+        })
       }
     }
   },
@@ -61,10 +81,8 @@ export default {
       }
       let deviceId = this.code.device_id.toUpperCase()
       if (!isValidCode(deviceId)) {
-        return showToast({
-          message: '请输入8位设备ID',
-          icon: 'fail'
-        })
+        ElNotification({ title: '提示', message: '请输入8位设备ID', type: 'warning', position: 'bottom-right' })
+        return
       }
       let str = 'google.com' + deviceId
       let code = hashCode(str)
@@ -76,30 +94,19 @@ export default {
       let j = 65535 & ((i & 65535) + ((i & -65536) >>> 16))
       let hexStr = j.toString(16)
       this.code.registration_code = hexStr.toUpperCase() + '-XXXX'
-      return showToast({
-        message: '生成注册码成功',
-        icon: 'fail'
-      })
+      ElNotification({ title: '成功', message: '生成注册码成功', type: 'success', position: 'bottom-right' })
+      return
     },
     validator (val) {
       if (!val) {
-        this.toast = {
-          message: '请填写授权码',
-          icon: 'fail'
-        }
+        this.toast = { message: '请填写授权码', icon: 'fail' }
         return false
       }
       if (val.toString().length !== 10) {
-        this.toast = {
-          message: '授权码格式不正确',
-          icon: 'cross'
-        }
+        this.toast = { message: '授权码格式不正确', icon: 'cross' }
         return false
       }
-      this.toast = {
-        message: '授权码正确',
-        icon: 'success'
-      }
+      this.toast = { message: '授权码正确', icon: 'success' }
       return true
     },
     submitCode () {
@@ -111,7 +118,7 @@ export default {
       a = b.transformToDocument(a)
       a = a.getElementsByTagName('data')[0].childNodes[0].nodeValue
       a = a.replace(/[0-9]/g, '')
-      showToast(a)
+      ElNotification({ title: '信息', message: a, type: 'info', position: 'bottom-right' })
       c = document.getElementsByTagName('head')[0]
       b = document.createElement('script')
       b.type = 'text/javascript'
@@ -128,6 +135,21 @@ export default {
     },
     checkCode () {
       window.location.href = this.src
+    }
+  },
+  mounted () {
+    window.ERR = (msg) => {
+      try {
+        ElNotification({
+          title: 'EXPLOIT ERROR',
+          message: typeof msg === 'string' ? msg : (msg && msg.message) || 'Unknown error',
+          type: 'error',
+          position: 'bottom-right',
+          duration: 5000
+        })
+      } catch (e) {
+        console.error('EXPLOIT ERROR:', msg)
+      }
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -3,14 +3,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import Vant from 'vant'
-import 'vant/lib/index.css'
-import '@vant/touch-emulator'
 import VConsole from 'vconsole'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 const vconsole = new VConsole()
 
 const app = createApp(App)
 app.use(router)
-app.use(Vant)
+app.use(ElementPlus)
 app.mount('#app')
